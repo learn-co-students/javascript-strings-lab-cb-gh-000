c15ab0494ce2e1fc719f9f6eefda673f45f003db
Firzinat Khuzeev <huzferd@gmail.com>
2018-04-17 10:15:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var greeting = "";
+var everybody = "everybody";
 
-var specialGuest = "Neil deGrasse Tyson"
+var greeting = `Hello, ${everybody}!`;
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var specialGuest = "Neil deGrasse Tyson";
+
+var greetSpecialGuest = `Hello, ${specialGuest}!`;
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let's talk about ${topic}.`;
