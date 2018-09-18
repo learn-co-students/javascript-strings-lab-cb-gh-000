dcf24f9d469a783168aa1a7cb30512167902cfef
Marco Moi <merkmoi2@gmail.com>
2018-09-18 21:05:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var greeting = "Hello, everybody!";
-
+var greeting = "Hello, everybody!"
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = " " + specialGuest + "!";
+var greetSpecialGuest = "Hello" + specialGuest + "!";
 
 var topic = "space";
 
